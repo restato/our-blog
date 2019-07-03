ea6dc18c991228149d1d89ac6840d08426c9569e
stato <direcision@gmail.com>
2019-07-03 21:19:58 +0900

    (#2) add router (navigation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
+    menuLinks:[
+       {
+          name:'home',
+          link:'/'
+       },
+       {
+          name:'page2',
+          link:'/page-2'
+       }
+    ],
     author: `Kyle Mathews`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
